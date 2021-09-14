375053f09addb89ad41ab3ff450a5de6233849b9
Pat Schloss <pschloss@umich.edu>
2021-09-14 10:13:56 -0400

    Add titles and format

--- august_october_2020_slope.R
+++ august_october_2020_slope.R
@@ -1,5 +1,6 @@
 library(tidyverse)
 library(showtext)
+library(ggtext)
 
 font_add_google(family="patua-one", "Patua One")
 font_add_google(family="roboto", "Roboto")
@@ -16,10 +17,26 @@ data <- read_csv("august_october_2020.csv") %>%
                           "South Africa" = "S. Africa",
                           "United Kingdom" = "UK",
                           "United States" = "USA")) %>%
-  pivot_longer(cols =c(august, october), names_to="month", values_to="percent")
+  pivot_longer(c(august, october), names_to="month", values_to="percent")
 
 data %>%
   ggplot(aes(x=month, y=percent, group=country, color=country)) +
-  geom_line()
+  geom_line() +
+  labs(title = "COVID-19 vaccination intent is decreasing globally",
+       subtitle = NULL,
+       caption = "<i>Base: 18,526 online adults aged 16-74 across 15 countries</i><br>Source: Ipsos",
+       tag = NULL, 
+       x = NULL,
+       y= "Pecent willing to receive vaccine",
+       color = NULL) +
+  theme(
+    plot.title = element_textbox_simple(family = "patua-one", size=28,
+                                        lineheight = 1,
+                                        margin = margin(b=10)),
+    plot.title.position = "plot",
+    plot.caption = element_markdown(hjust = 0, color="gray",
+                                    margin= margin(t=10)),
+    plot.caption.position = "plot"
+  )
 
 ggsave("august_october_2020_slope.tiff", width=5, height=5)
