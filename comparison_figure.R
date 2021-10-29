959afed939a432c84897f4bc67db8b6d2fdf78c3
Pat Schloss <pschloss@umich.edu>
2021-10-29 14:19:40 -0400

    Filter data to get 15 countries of interest for 2021-10-28

--- comparison_figure.R
+++ comparison_figure.R
@@ -3,4 +3,15 @@ library(tidyverse)
 owid <- read_csv("https://covid.ourworldindata.org/data/owid-covid-data.csv") %>%
   select(location, date,
          all_vax = people_vaccinated_per_hundred,
-         fully_vax = people_fully_vaccinated_per_hundred)
+         fully_vax = people_fully_vaccinated_per_hundred) %>%
+  filter((location == "Australia" | location == "Brazil" |
+            location == "Canada" | location == "China" |
+            location == "France" | location == "Germany" |
+            location == "India" | location == "Italy" |
+            location == "Japan" | location == "Mexico" |
+            location == "South Africa" | location == "South Korea" |
+            location == "Spain" | location == "United Kingdom" |
+            location == "United States") &
+           date == "2021-10-28") %>%
+  drop_na(fully_vax)
+
