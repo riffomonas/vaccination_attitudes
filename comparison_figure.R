0bed39115831c015e5158abed17d5867949af933
Pat Schloss <pschloss@umich.edu>
2021-10-29 15:01:06 -0400

    Figuring out why only 8 of 15 countries have data for 2021-10-28

--- comparison_figure.R
+++ comparison_figure.R
@@ -4,14 +4,16 @@ owid <- read_csv("https://covid.ourworldindata.org/data/owid-covid-data.csv") %>
   select(location, date,
          all_vax = people_vaccinated_per_hundred,
          fully_vax = people_fully_vaccinated_per_hundred) %>%
-  filter((location == "Australia" | location == "Brazil" |
-            location == "Canada" | location == "China" |
-            location == "France" | location == "Germany" |
-            location == "India" | location == "Italy" |
-            location == "Japan" | location == "Mexico" |
-            location == "South Africa" | location == "South Korea" |
-            location == "Spain" | location == "United Kingdom" |
-            location == "United States") &
-           date == "2021-10-28") %>%
-  drop_na(fully_vax)
+  filter(location == "Brazil") %>%
+  drop_na(fully_vax) %>% slice_max(date)
 
+  # filter((location == "Australia" | location == "Brazil" |
+  #           location == "Canada" | location == "China" |
+  #           location == "France" | location == "Germany" |
+  #           location == "India" | location == "Italy" |
+  #           location == "Japan" | location == "Mexico" |
+  #           location == "South Africa" | location == "South Korea" |
+  #           location == "Spain" | location == "United Kingdom" |
+  #           location == "United States") &
+  #          date == "2021-10-28") %>%
+  # drop_na(fully_vax)
