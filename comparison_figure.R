292867305beb9e0eeeb37202b659c9b55b2b26f2
Pat Schloss <pschloss@umich.edu>
2021-10-29 16:27:32 -0400

    Join Ipsos and OWID data frames together

--- comparison_figure.R
+++ comparison_figure.R
@@ -1,19 +1,19 @@
 library(tidyverse)
 
 owid <- read_csv("https://covid.ourworldindata.org/data/owid-covid-data.csv") %>%
-  select(location, date,
+  select(country = location,
+         date,
          all_vax = people_vaccinated_per_hundred,
          fully_vax = people_fully_vaccinated_per_hundred) %>%
-  filter((location == "Australia" | location == "Brazil" |
-            location == "Canada" | location == "China" |
-            location == "France" | location == "Germany" |
-            location == "India" | location == "Italy" |
-            location == "Japan" | location == "Mexico" |
-            location == "South Africa" | location == "South Korea" |
-            location == "Spain" | location == "United Kingdom" |
-            location == "United States")) %>% 
   drop_na(fully_vax) %>%
-  group_by(location) %>%
+  group_by(country) %>%
   slice_max(date, n=1) %>%
   ungroup() %>%
   select(-date)
+
+ipsos <- read_csv("august_october_2020.csv") %>%
+  rename(country = X.1,
+         percent_august = "Total Agree - August 2020",
+         percent_october = "Total Agree - October 2020")
+  
+ipsos_owid <- inner_join(owid, ipsos, by="country")
