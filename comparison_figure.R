9465d9a42a5666756ce18d8d648bbb03c880f0ad
Pat Schloss <pschloss@umich.edu>
2021-10-29 15:51:15 -0400

    Get the most recent data with data for % fully vacinated from countries

--- comparison_figure.R
+++ comparison_figure.R
@@ -4,16 +4,16 @@ owid <- read_csv("https://covid.ourworldindata.org/data/owid-covid-data.csv") %>
   select(location, date,
          all_vax = people_vaccinated_per_hundred,
          fully_vax = people_fully_vaccinated_per_hundred) %>%
-  filter(location == "Brazil") %>%
-  drop_na(fully_vax) %>% slice_max(date)
-
-  # filter((location == "Australia" | location == "Brazil" |
-  #           location == "Canada" | location == "China" |
-  #           location == "France" | location == "Germany" |
-  #           location == "India" | location == "Italy" |
-  #           location == "Japan" | location == "Mexico" |
-  #           location == "South Africa" | location == "South Korea" |
-  #           location == "Spain" | location == "United Kingdom" |
-  #           location == "United States") &
-  #          date == "2021-10-28") %>%
-  # drop_na(fully_vax)
+  filter((location == "Australia" | location == "Brazil" |
+            location == "Canada" | location == "China" |
+            location == "France" | location == "Germany" |
+            location == "India" | location == "Italy" |
+            location == "Japan" | location == "Mexico" |
+            location == "South Africa" | location == "South Korea" |
+            location == "Spain" | location == "United Kingdom" |
+            location == "United States")) %>% 
+  drop_na(fully_vax) %>%
+  group_by(location) %>%
+  slice_max(date, n=1) %>%
+  ungroup() %>%
+  select(-date)
