afbc294673dd460c2272a6ed41f5cf6c5454e957
Pat Schloss <pschloss@umich.edu>
2021-10-29 12:02:55 -0400

    Select and rename relevant columns from data frame

--- comparison_figure.R
+++ comparison_figure.R
@@ -1,3 +1,6 @@
 library(tidyverse)
 
-owid <- read_csv("https://covid.ourworldindata.org/data/owid-covid-data.csv")
+owid <- read_csv("https://covid.ourworldindata.org/data/owid-covid-data.csv") %>%
+  select(location, date,
+         all_vax = people_vaccinated_per_hundred,
+         fully_vax = people_fully_vaccinated_per_hundred)
